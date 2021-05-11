48b05acbd6c10a938b7a35c743a650a5e2ffc742
Anchal Pandey <73632448+code1511@users.noreply.github.com>
2021-05-11 19:37:55 +0530

    Create props_INReact.js
    how to use props in react js

---
+++ Learn_React/props_INReact.js
@@ -0,0 +1,11 @@
+//using props as functional component
+
+import React from 'react'
+
+export default function greet(props) {
+    return (
+        <div>
+            <h1>heyooo {props.name} </h1>
+        </div>
+    )
+}
